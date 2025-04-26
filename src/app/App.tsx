8a37f6ed986d5a5fc0313b2486e46b0978a27582
Mohammad Amin Rizki - Partner DAD <90122214@unitedtractors.com>
2025-04-27 01:33:08 +0700

    ⚡️ App render

---
+++ src/app/App.tsx
@@ -0,0 +1,36 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+
+// Import the generated route tree
+import { routeTree } from "../routeTree.gen.ts";
+
+import "../styles.css";
+
+const queryClient = new QueryClient();
+
+// Create a new router instance
+const router = createRouter({
+  routeTree,
+  context: { queryClient },
+  defaultPreload: "intent",
+  scrollRestoration: true,
+  defaultStructuralSharing: true,
+  defaultPreloadStaleTime: 0,
+});
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="web-wheater-apps">
+        <RouterProvider router={router} />
+      </div>
+    </QueryClientProvider>
+  );
+}
