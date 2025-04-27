3131ca4b8944cf84f615330be0e4030cda027195
Mohammad Amin Rizki - Partner DAD <90122214@unitedtractors.com>
2025-04-27 20:47:21 +0700

    feat: 🎉 add tanstack query devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,18 @@
+import React from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
-	component: () => (
-		<>
-			<Outlet />
-			<TanStackRouterDevtools />
-		</>
-	),
+  component: () => (
+    <React.Fragment>
+      <Outlet />
+      {import.meta.env.DEV && (
+        <React.Fragment>
+          <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right" />
+          <TanStackRouterDevtools />
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  ),
 });
